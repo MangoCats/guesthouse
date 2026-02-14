aa51559e1d701ef1cc304336632351b83a4d5a3b
Mangocats <mangocats@gmail.com>
2026-02-13 23:28:19 -0500

    Shorten east counter depth from 72 to 42 inches

--- floorplan/constants.py
+++ floorplan/constants.py
@@ -60,7 +60,7 @@ KITCHEN_CTR_DEPTH = 24.0 / 12.0    # 24" kitchen counter depth N-S
 NORTH_CTR_LENGTH = 38.0 / 12.0     # 38" north wall counter E-W
 NORTH_CTR_DEPTH = 24.0 / 12.0      # 24" north wall counter depth N-S
 EAST_CTR_LENGTH = 30.0 / 12.0      # 30" east counter E-W along W9-W10
-EAST_CTR_DEPTH = 72.0 / 12.0       # 72" east counter depth N-S
+EAST_CTR_DEPTH = 42.0 / 12.0       # 42" east counter depth N-S
 EAST_CTR_RADIUS = 12.0 / 12.0      # 12" south corner radius
 
 # IW6 partition
