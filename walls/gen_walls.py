4bc5a735e399c26dbd425950ab64a405e8851903
Mangocats <mangocats@gmail.com>
2026-02-14 14:45:22 -0500

    Sync O6 position in walls/gen_walls.py

--- walls/gen_walls.py
+++ walls/gen_walls.py
@@ -145,8 +145,8 @@ def _compute_openings(pts, outline_segs, layout):
     # --- O5 & O6: F9-F10, horizontal ---
     idx = seg_map[("F9", "F10")]
     # O6 edges (computed first so O5 can reference the 78" gap)
-    o6_e = pts["F10"][0] - 4.0 / 12.0
-    o6_w = pts["F10"][0] - 48.0 / 12.0
+    o6_e = pts["F10"][0] - 10.0 / 12.0
+    o6_w = pts["F10"][0] - 54.0 / 12.0
     # O5: 6' opening, 78" west of O6
     o5_e = o6_w - 78.0 / 12.0
     o5_w = o5_e - 6.0
