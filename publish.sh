02f713b11bcc0256386bc181e471edad4719014d
Mangocats <mangocats@gmail.com>
2026-02-15 11:42:16 -0500

    publish more svg files

--- publish.sh
+++ publish.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 
 cp floorplan/floorplan.svg ../www/adu/floorplan.svg
-git -C ../www add adu/floorplan.svg
-git -C ../www commit -m "update floorplan"
+cp     walls/walls.svg     ../www/adu/walls.svg
+cp     walls/all_walls.svg ../www/adu/all_walls.svg
+cp    survey/path_area.svg ../www/adu/path_area.svg
+git -C ../www add adu/floorplan.svg adu/walls.svg adu/all_walls.svg adu/path_area.svg
+git -C ../www commit -m "update floorplan and related files"
 git -C ../www push
