a75b4d069d333252e4f212bcff6e19f54ae97244
Mangocats <mangocats@gmail.com>
2026-02-16 23:06:06 -0500

    Move RO1 48in (4ft) west along IW1

--- floorplan/constants.py
+++ floorplan/constants.py
@@ -67,7 +67,7 @@ EAST_CTR_RADIUS = 12.0 / 12.0      # 12" south corner radius
 
 # IW1 rough opening
 RO1_OFFSET_W_IW4 = 64.0 / 12.0    # 64" west of IW4 west face (legacy)
-RO1_OFFSET_E_IW9 = 76.0 / 12.0    # 76" east of IW9 east face
+RO1_OFFSET_E_IW9 = 28.0 / 12.0    # 28" east of IW9 east face
 IW1_RO_WIDTH = 38.0 / 12.0        # 38" opening width E-W
 
 # IW2 rough opening
