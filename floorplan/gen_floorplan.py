792d6f9014ea29fe83e74841d150e22d43be9ea7
Mangocats <mangocats@gmail.com>
2026-02-18 08:53:03 -0500

    DB: move ROCKER 8in west

--- floorplan/gen_floorplan.py
+++ floorplan/gen_floorplan.py
@@ -1383,7 +1383,7 @@ def _render_furniture(out, data, layout, minik=False, db=False):
         #         center N-S between IW1 and fridge door arc southern extent
         _ro1_w = layout.iw2.e + RO1_OFFSET_E_IW2
         _ro1_e = _ro1_w + IW1_RO_WIDTH
-        rk_cx = (db_w + _ro1_e) / 2
+        rk_cx = (db_w + _ro1_e) / 2 - 8.0 / 12.0
         # Recompute fridge door arc southern extent
         _st_w = layout.iw2.e + NORTH_CTR_LENGTH + KITCHEN_APPL_GAP
         _st_e = _st_w + STOVE_WIDTH
