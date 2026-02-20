aa8a75f435d94093b1a29bdcbab7bd6d6a5f5371
Mangocats <mangocats@gmail.com>
2026-02-20 09:56:12 -0500

    Add site plan generator to gen_all.py

--- gen_all.py
+++ gen_all.py
@@ -17,6 +17,7 @@ _SCRIPTS = [
     os.path.join(_DIR, "span", "gen_span_minmax.py"),
     os.path.join(_DIR, "span", "gen_span_min.py"),
     os.path.join(_DIR, "roof", "gen_roof.py"),
+    os.path.join(_DIR, "site", "gen_site_plan.py"),
 ]
 
 
